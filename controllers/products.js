482bb5dd86dac95765121819106a989bc5d54840
NyanLinHtet1999 <andydufresne323@gmail.com>
2024-01-14 18:06:31 +0630

    numeric filter

--- controllers/products.js
+++ controllers/products.js
@@ -11,7 +11,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-     const {featured , company , name , sort , fields } = req.query;
+     const {featured , company , name , sort , fields ,numericFilters} = req.query;
      let queryObject = {}
      if(featured) {
           queryObject.featured = featured === "true" ? true : false;
@@ -22,8 +22,28 @@ const getAllProducts = async (req, res) => {
      if(name) {
           queryObject.name = {$regex : name, $options : "i" }
      }
+     if (numericFilters) {
+          const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+          };
+          const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+          let filters = numericFilters.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+          );
+          const options = ['price', 'rating'];
+          filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-');
+            if (options.includes(field)) {
+              queryObject[field] = { [operator]: Number(value) };
+            }
+          });
+        }
      let result =  Product.find(queryObject);
-     console.log(sort)
      if(sort) {
          let sortPara = sort.replace(",", " ");
 
